79224e49aee65baecfd1608b440dd8b90b4d0108
Millew-dev <millianmille11@gmail.com>
2025-07-19 21:36:14 +0300

    Implement user authentication (login, logout, register)

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,14 +1,15 @@
-# relationship_app/models.py
 from django.db import models
+from django.contrib.auth.models import User
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
 
-class Library(models.Model):
-    name = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    library = models.ForeignKey(Library, on_delete=models.CASCADE, related_name='books')
-    publication_year = models.PositiveIntegerField()
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
