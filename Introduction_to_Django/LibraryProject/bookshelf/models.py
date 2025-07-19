a16c7bc1090029f5882d911a66ede37448f43202
Millew-dev <millianmille11@gmail.com>
2025-07-19 18:06:16 +0300

    Revisions done on models.py

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)  # Max 200 chars
+    author = models.CharField(max_length=100)  # Max 100 chars
+    publication_year = models.IntegerField()  # Year as integer
+
+    def _str_(self):
+        return f"{self.title} by {self.author} ({self.publication_year})"
